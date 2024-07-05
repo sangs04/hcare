a0d5ed15ff1397c6f9ddf611d9ede8dc38aee0f9
sangs04 <117534645+sangs04@users.noreply.github.com>
2024-07-05 16:52:45 +0530

    api files added

---
+++ api/routes/orders.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const router=express.Router();
+const OrdersController=require('../controllers/orders')
+const checkAuth = require("../middleware/check-auth")
+
+router.get('/',checkAuth,OrdersController.orders_get_all)
+router.post('/',checkAuth,OrdersController.orders_create_order)
+router.get('/:orderId',checkAuth,OrdersController.order_get_order)
+router.delete('/:orderId',checkAuth,OrdersController.orders_delete_order)
+
+module.exports=router;
